b41a57d12a0ef29bd7e845dac581ebe4599ec2de
Tony Phan <tonyphanmc@hotmail.com>
2024-04-06 15:34:07 -0400

    Add method to distribute cards and points attributes

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,35 +1,40 @@
 import java.util.ArrayList;
 import java.util.Random;
-
+//Points will be earned depending on difference of ranks played.
 public class CardGame {
     ArrayList<Card> deck;
+    ArrayList<Card> playerDeck;
+    ArrayList<Card> computerDeck;
+    int playerPoint;
+    int computerPoint;
+
     CardGame(){
         buildDeck();
         shuffleDeck();
+        distributeDeck();
     }
 
     public static void main(String[] args){
         CardGame game = new CardGame();
-        ArrayList<Card> deck = game.deck;
-        for (Card element: deck){
-            System.out.println(element);
-        }
+
     }
 
-    private class Card{
+
+    private class Card {
         private String suit;
         private String rank;
-        public Card(String suit, String rank){
+
+        public Card(String suit, String rank) {
             this.suit = suit;
             this.rank = rank;
         }
-        public String toString(){
+
+        public String toString() {
             return rank + "-" + suit;
         }
-
     }
 
-    public void buildDeck(){
+    private void buildDeck(){
         this.deck = new ArrayList<>(52);
         String[] suitArr = {"Heart", "Diamond", "Club", "Spade"};
         String[] rankArr = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
@@ -40,7 +45,7 @@ public class CardGame {
         }
     }
 
-    public void shuffleDeck(){
+    private void shuffleDeck(){
         Random random = new Random();
         for (int i = 0; i < 260; i++){
             int index1 = random.nextInt(52);
@@ -51,6 +56,15 @@ public class CardGame {
             Card temp = deck.get(index1);
             deck.set(index1, deck.get(index2));
             deck.set(index2, temp);
+        }
+    }
+
+    private void distributeDeck(){
+        playerDeck = new ArrayList<>(26);
+        computerDeck = new ArrayList<>(26);
+        while(!deck.isEmpty() && deck.size() != 1){
+            playerDeck.add(deck.remove(deck.size() - 1));
+            computerDeck.add(deck.remove(deck.size() - 1));
         }
     }
 }
